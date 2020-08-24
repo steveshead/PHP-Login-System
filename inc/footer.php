d7d2e5096aac430af4e3a1a94709f2f26fe95a30
steveshead <steve@steve-shead.com>
2020-08-24 12:49:16 -0700

    Added includes and forms

---
+++ inc/footer.php
@@ -0,0 +1,8 @@
+
+        <!-- JQuery -->
+        <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+        <!-- UIkit JS -->
+        <script src="https://cdn.jsdelivr.net/npm/uikit@3.5.6/dist/js/uikit.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/uikit@3.5.6/dist/js/uikit-icons.min.js"></script>
+    </body>
+</html>
