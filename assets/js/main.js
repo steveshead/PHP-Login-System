b890f5b2cdca1dbeb998397b1fd1f656328570d3
steveshead <steve@steve-shead.com>
2020-08-25 14:49:15 -0700

    Added first Ajax page

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,17 +4,17 @@ $(document)
 
     var _form = $(this);
     var _error = $(".js-error", _form);
-    var data = {
+    var dataObj = {
         email: $("input[type='email']", _form).val(),
         password: $("input[type='password']", _form).val()
     }
 
-    if(data.email.length < 6) {
+    if(dataObj.email.length < 6) {
         _error
             .text("Please enter a valid email address")
             .show();
         return false;
-    } else if (data.password.length < 8){
+    } else if (dataObj.password.length < 8){
         _error
             .text("Please enter a pass phrase that is at least 11 characters long.")
             .show();
@@ -23,6 +23,30 @@ $(document)
 
     // Assuming the code gets this far, we can start the ajax process
     _error.hide();
+    $.ajax({
+        type: 'POST',
+        url: '/ajax/register.php',
+        dataObj: dataObjObj,
+        dataObjType: 'json',
+        async: true,
+    })
+        .done(function ajaxDone(dataObj) {
+            // Whatever dataObj is
+            console.log(dataObj);
+            if(dataObj.redirect !== undefined) {
+                // window.location = dataObj.redirect;
+            }
+
+            alert(dataObj.name);
+        })
+        .fail(function ajaxFailed(e) {
+            // This failed
+            console.log(e);
+        })
+        .always(function ajaxAlwaysDoThis(dataObj) {
+            // Always do
+            console.log('Always');
+        })
 
     return false;
 })
