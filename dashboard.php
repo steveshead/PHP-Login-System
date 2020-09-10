38e9437bb0c4e8e2f8dba1b95afa764bc1c72ee1
steveshead <steve@steve-shead.com>
2020-09-10 09:55:34 -0700

    Styled the table on dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -14,34 +14,36 @@
 
   	<div class="uk-section uk-container">
   		<h3 style="margin-bottom:0 !important">Hello and welcome <?php echo $User->firstname; ?>.</h3>
-			<h5 style="margin-top:5px">You registered on <?php echo date('l jS \of F Y h:i A', strtotime($User->reg_time)); ?></h5>
+			<h5 style="margin-top:5px">You registered on <?php echo date('l, jS \of F, Y \a\t h:i A', strtotime($User->reg_time)); ?></h5>
         <p>Below are your user details.</p>
 
-        <div class="uk-overflow-auto">
-            <table class="uk-table uk-table-small uk-table-hover">
-                <tbody>
-                    <tr>
-                        <th class="uk-width-small">First Name</th>
-                        <td><?php echo $User->firstname; ?></td>
-                    </tr>
-                    <tr>
-                        <th class="uk-width-small">Last Name</th>
-                        <td><?php echo $User->lastname; ?></td>
-                    </tr>
-                    <tr>
-                        <th class="uk-width-small">Username</th>
-                        <td><?php echo $User->username; ?></td>
-                    </tr>
-                    <tr>
-                        <th class="uk-width-small">Email</th>
-                        <td><?php echo $User->email; ?></td>
-                    </tr>
-                    <tr>
-                        <th class="uk-width-small">Date Registered</th>
-                        <td><?php echo date('jS \of F, Y', strtotime($User->reg_time)); ?></td>
-                    </tr>
-                </tbody>
-            </table>
+        <div class="uk-grid">
+            <div class="uk-overflow-auto">
+                <table class="uk-table uk-table-small uk-table-striped" style="border:1px solid #eee;">
+                    <tbody>
+                        <tr>
+                            <th class="uk-width-small">First Name</th>
+                            <td><?php echo $User->firstname; ?></td>
+                        </tr>
+                        <tr>
+                            <th class="uk-width-small">Last Name</th>
+                            <td><?php echo $User->lastname; ?></td>
+                        </tr>
+                        <tr>
+                            <th class="uk-width-small">Username</th>
+                            <td><?php echo $User->username; ?></td>
+                        </tr>
+                        <tr>
+                            <th class="uk-width-small">Email</th>
+                            <td><?php echo $User->email; ?></td>
+                        </tr>
+                        <tr>
+                            <th class="uk-width-small">Date Registered</th>
+                            <td><?php echo date('jS \of F, Y', strtotime($User->reg_time)); ?></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
 
         <a class="uk-button uk-button-default uk-margin" href="logout.php">Logout</a>
